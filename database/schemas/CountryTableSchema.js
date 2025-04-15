e58f0185c68383c4264091d41baec515d66187c9
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-04-15 09:51:41 +0530

    add change of country module

--- database/schemas/CountryTableSchema.js
+++ database/schemas/CountryTableSchema.js
@@ -1,31 +1,30 @@
 import mongoose from "mongoose";
 import mongooseDelete from "mongoose-delete";
 
-const schema = new mongoose.Schema(
-  {
+const schema = new mongoose.Schema({
     name: {
-      type: String,
-      required: true,
-      maxLength: 30,
-      unique: true,
+        type: String,
+        required: true,
+        maxLength: 30,
+        unique: true,
     },
     code: {
-      type: String,
-      required: true,
-      maxLength: 2,
-      unique: true,
+        type: String,
+        required: true,
+        maxLength: 2,
+        unique: true,
     },
     currency: {
-      type: String,
-      required: true,
-      maxLength: 3,
-      unique: true,
+        type: String,
+        required: true,
+        maxLength: 3,
+        unique: true,
     },
     symbol: {
-      type: String,
-      required: true,
-      maxLength: 5,
-      unique: true,
+        type: String,
+        required: true,
+        maxLength: 5,
+        unique: true,
     },
     deletedAt: {
       type: Date,

ebcf95016cf6526b28c0b9597c08592eeb6fcd1b
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-04-28 18:02:58 +0530

    add file

--- config/database.js
+++ config/database.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
         mongoose.set('debug', false);
-        console.error('Connected Successfully');
     } catch (error) {
         console.error('Error while connecting with MongoDB:', error);
         process.exit(1);
